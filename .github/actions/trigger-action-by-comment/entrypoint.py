1bcbed6ac85530a59a38bc0c915de3fbecd07ad6
harupy <hkawamura0130@gmail.com>
2020-06-17 15:06:12 +0900

    Add input pull_number

--- .github/actions/trigger-action-by-comment/entrypoint.py
+++ .github/actions/trigger-action-by-comment/entrypoint.py
@@ -1,8 +1,15 @@
+import os
+
 import requests
 
 
+def get_action_input(name):
+    return os.getenv(f"INPUT_{name.upper()}")
+
+
 def main():
-    print(requests.__version__)
+    pull_num = get_action_input("pull_number")
+    print(pull_num)
 
 
 if __name__ == "__main__":
