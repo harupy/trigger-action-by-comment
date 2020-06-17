88676398efb94b3a6db67823edd06f32f22022e9
harupy <hkawamura0130@gmail.com>
2020-06-17 15:10:34 +0900

    parse job

--- .github/actions/trigger-action-by-comment/entrypoint.py
+++ .github/actions/trigger-action-by-comment/entrypoint.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import requests
 
@@ -7,10 +8,15 @@ def get_action_input(name):
     return os.getenv(f"INPUT_{name.upper()}")
 
 
+def parse_job(comment):
+    return re.search(r"trigger (.+)").group(1)
+
+
 def main():
     pull_num = get_action_input("pull_number")
     comment = get_action_input("comment")
-    print(pull_num, comment)
+    job = parse_job(comment)
+    print(pull_num, comment, job)
 
 
 if __name__ == "__main__":
